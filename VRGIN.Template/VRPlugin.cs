04254cb832764baa3f74d1aeb7b7dfe29f134bb2
obtdai <781016+obtdai@users.noreply.github.com>
2018-05-03 01:06:32 +0900

    update VRLoading

--- VRGIN.Template/VRPlugin.cs
+++ VRGIN.Template/VRPlugin.cs
@@ -1,10 +1,8 @@
 ﻿using IllusionPlugin;
+using UnityEngine;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
+using System.Collections;
 using System.Xml.Serialization;
 using VRGIN.Core;
 using VRGIN.Helpers;
@@ -47,58 +45,17 @@ namespace VRGIN.Template
 
             if (vrActivated || (!vrDeactivated && SteamVRDetector.IsRunning))
             {
-                // Boot VRManager!
-                // Note: Use your own implementation of GameInterpreter to gain access to a few useful operatoins
-                // (e.g. characters, camera judging, colliders, etc.)
-                VRManager.Create<GameInterpreter>(CreateContext("VRContext.xml"));
-                VR.Manager.SetMode<GenericSeatedMode>();
-
+				VRLoader.Create(true);
             }
 			else
 			{
-				UnityEngine.VR.VRSettings.enabled = false;
-				UnityEngine.VR.VRSettings.LoadDeviceByName("None");
+				VRLoader.Create(false);
             }
         }
 
-        #region Helper code
 
-        private IVRManagerContext CreateContext(string path) {
-            var serializer = new XmlSerializer(typeof(ConfigurableContext));
 
-            if(File.Exists(path))
-            {
-                // Attempt to load XML
-                using (var file = File.OpenRead(path))
-                {
-                    try
-                    {
-                        return serializer.Deserialize(file) as ConfigurableContext;
-                    }
-                    catch (Exception e)
-                    {
-                        VRLog.Error("Failed to deserialize {0} -- using default", path);
-                    }
-                }
-            }
 
-            // Create and save file
-            var context = new ConfigurableContext();
-            try
-            {
-                using (var file = new StreamWriter(path))
-                {
-                    file.BaseStream.SetLength(0);
-                    serializer.Serialize(file, context);
-                }
-            } catch(Exception e)
-            {
-                VRLog.Error("Failed to write {0}", path);
-            }
-
-            return context;
-        }
-        #endregion
 
         #region Unused
         public void OnApplicationQuit() { }
